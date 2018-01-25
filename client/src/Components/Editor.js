7e8367a849f4f473e8448324c648e657ed7f40f3
Nathaniel Sena <nate.sena1@gmail.com>
2018-01-24 17:16:37 -0800

    sending some editor data to db

--- client/src/Components/Editor.js
+++ client/src/Components/Editor.js
@@ -1,39 +1,18 @@
 import React from 'react'
-import {Editor, EditorState, RichUtils } from 'draft-js'
-
-const EditorStyle = {
-    border: '1px solid black'
-}
+import { Editor } from 'react-draft-wysiwyg';
+import '../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 class EditorComponent extends React.Component{
-    state = {
-        editorState: EditorState.createEmpty()
-    }
-    onChange = (editorState) => {
-        this.setState({
-          editorState,
-        });
-    }
-    onBoldClick(evt) {
-        evt.preventDefault()
-        this.onChange(RichUtils.toggleInlineStyle(
-          this.state.editorState,
-          'BOLD'
-        ));
-      }
+    
     render(){
-        return(
-            
-            <div>
-                <h1>Editor</h1>
-                <button onClick={this.onBoldClick.bind(this)}>Bold Nah</button>
-                <div style={EditorStyle}>
-                    <Editor 
-                    editorState={this.state.editorState}
-                    onChange={this.onChange}
-                    />
-                </div>
-            </div>
+        return( 
+            <Editor
+                editorState={this.props.editorState}
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorClassName="editorClassName"
+                onEditorStateChange={this.props.onChange}
+            />
         )
     }
 }
--- client/src/Components/Upload.js
+++ client/src/Components/Upload.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import axios from 'axios'
+// need to feed in the raw data from the editor component into here
 import EditorComponent from './Editor.js'
+import { EditorState } from 'draft-js';
+//potentially add photo uploads here as well
+
+const EditorStyle = {
+    border: "5px solid black"
+}
+
+const InputStyle = {
+    border: "none",
+    margin: "20px 0 20px 0",
+
+}
+const TextAreaStyle = {
+    border: "none",
+    margin: "20px 0 10px 0"
+}
+
+const SelectStyle = {
+    margin: "10px 0 10px 0",
+    backgroundColor: "gray",
+    fontSize: "1.5em",
+    padding: "5px"
+}
+
 
 class Upload extends React.Component{
     state = {
         title: '',
         ProjectDescription: '',
-        type: 'WebDevelopment'
+        type: 'WebDevelopment',
+        editorState: EditorState.createEmpty(),
     }
     onChange(evt){
         this.setState({
@@ -18,12 +44,22 @@ class Upload extends React.Component{
             type: evt.target.value
         })
     }
+    onEditorStateChange(editorState) {
+        this.setState({
+          editorState
+        })
+    }
+    // get the JSON object and send it post body
     submit(evt){
         evt.preventDefault()
-        console.log('tried to submit ' + this.state.title + ', ' + this.state.ProjectDescription)
+        var bodyJSONData = this.state.editorState.getCurrentContent()
+        console.log("bodyJSONData: ",bodyJSONData)
+        console.log('tried to submit ' + this.state.title + ', ' + this.state.ProjectDescription + ', ' + this.state.editorState)
         axios.post('/api/Blog', {
             title: this.state.title,
-            description: this.state.ProjectDescription
+            description: this.state.ProjectDescription,
+            type: this.state.type,
+            body: bodyJSONData
         })
         .then((res)=>{
             console.log('res', res)
@@ -32,22 +68,21 @@ class Upload extends React.Component{
     render(){
         let editor = null
         if(this.state.type !== 'Ideas'){
-            editor = <EditorComponent/>
+            editor = <EditorComponent
+                    style={EditorStyle} onChange={this.onEditorStateChange.bind(this)} editorState={this.state.editorState}/>
         }
         return(
             <div>
                 <h1>Upload Here</h1>
                 <form onSubmit={this.submit.bind(this)}>
                     <div>
-                        <label>Name</label>
-                        <input type='text' name='title' value={this.state.value} onChange={this.onChange.bind(this)}/>
+                        <input style={InputStyle} type='text' name='title' placeholder="Name" value={this.state.value} onChange={this.onChange.bind(this)}/>
                     </div>
                     <div>
-                        <label>Project Description</label>
-                        <textarea name='ProjectDescription' value={this.state.value} onChange={this.onChange.bind(this)}></textarea>
+                        <textarea style={TextAreaStyle} name='ProjectDescription' placeholder="Project Description" value={this.state.value} onChange={this.onChange.bind(this)}></textarea>
                     </div>
 
-                    <select onChange={this.onSelectChange.bind(this)}>
+                    <select style={SelectStyle} onChange={this.onSelectChange.bind(this)}>
                         <option value="WebDevelopment">Web Development</option>
                         <option value="ProductDesign">Product Design</option>
                         <option value="Blog">Blog</option>
