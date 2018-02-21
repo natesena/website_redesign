ee0ad72197a1bfe579a0ff8b55e590d455c0d590
Nathaniel Sena <nate.sena1@gmail.com>
2018-02-20 21:18:43 -0800

    aframe planes working

--- client/src/Components/Aframe/AframeComponents/AframePost.js
+++ client/src/Components/Aframe/AframeComponents/AframePost.js
@@ -2,21 +2,22 @@ import 'aframe'
 import React from 'react'
 
 class AframePost extends React.Component{
+    //reverseRotation places a second plane in the same spot as the first plane carrying the post
+    //this second plane is angled the exact opposite as to make the post visible from both sides
     reverseRotation(rotationString){
+        // console.log(`input rotation string ${this.props.index}: `, rotationString)
         var coordinates = rotationString.split(' ')
-        if(coordinates[1] === '0'){
-            return coordinates.join(' ')
-        }
-        coordinates[1] = "-" + coordinates[1]
+        coordinates[1] = coordinates[1] - 180
         var returnCoordinates = coordinates.join(' ')
-        console.log("returnCoordinates: ", returnCoordinates)
+        // console.log(`returnCoordinates ${this.props.index}: `, returnCoordinates)
         return returnCoordinates
     }
     render(){
         return(
             <a-entity>
-                <a-plane height="2" width="2" color="#CCBBAA"position={this.props.position} rotation={this.props.rotation}></a-plane>
-                <a-plane height="2" width="2" color="#CCBBAA"position={this.props.position} rotation={this.reverseRotation(this.props.rotation)}></a-plane>
+                <a-text value={`${this.props.index}`} color="#FFAABB" position={this.props.position} rotation={this.props.rotation}></a-text>
+                <a-plane height="2" width="2" color="#CCBBAA" position={this.props.position} rotation={this.props.rotation}></a-plane>
+                <a-plane height="2" width="2" color="#CCBBAA" position={this.props.position} rotation={this.reverseRotation(this.props.rotation)}></a-plane>
             </a-entity>
         )
     }
--- client/src/Components/Aframe/HomeScene.js
+++ client/src/Components/Aframe/HomeScene.js
@@ -7,20 +7,23 @@ class AframeHome extends React.Component{
     state={
 
     }
+    //calculatePosition returns the location of where an aframe post should be.
     calculatePosition(index, length, rad, wid){
         console.log('calc pos: ', index, length, rad, wid)
         var angleBetweenPosts = 360/length
+        angleBetweenPosts = (Math.PI/180) * angleBetweenPosts
         //calculate equidistance given that radius
         var xCoordinate = rad * Math.sin(index*angleBetweenPosts)
-        var zCoordinate = rad * Math.cos(index*angleBetweenPosts)
+        var zCoordinate = -(rad * Math.cos(index*angleBetweenPosts))
         //if not enough room between posts, increase the radius
         var coordinates = `${xCoordinate} 1 ${zCoordinate}`
-        console.log(coordinates)
-        return coordinates
-        
+        console.log(`coordinates of ${index}: `,coordinates)
+        return coordinates 
     }
     calculateRotation(index,length){
         var yAxisRotation = (360/length) * index
+
+        console.log(`homescene calculated rotation of ${index}: `, yAxisRotation)
         return `0 ${yAxisRotation} 0`
     }
     render(){
@@ -29,9 +32,8 @@ class AframeHome extends React.Component{
                 <a-sky src="http://blog.topazlabs.com/wp-content/uploads/2013/07/Screen-Shot-2013-12-11-at-10.42.18-AM.png"></a-sky>
                 <a-circle color="#CCC" radius="20" position="0 -3 0" rotation="-90 0 0"></a-circle>
                 {this.props.posts.map((post, index)=>{
-                    return <AframePost key={post._id} position={this.calculatePosition(index, this.props.posts.length, 5, 3)} rotation={this.calculateRotation(index, this.props.posts.length)}/>
+                    return <AframePost key={post._id} index={index} position={this.calculatePosition(index, this.props.posts.length, 5, 3)} rotation={this.calculateRotation(index, this.props.posts.length)}/>
                 })}
-                <a-text value={`${this.props.posts.length}`} color="#FFAABB" position="0 1 -3"></a-text>
             </a-scene>
         )
     }
