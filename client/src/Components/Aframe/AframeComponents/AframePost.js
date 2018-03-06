f8eee817bd43f3ccf254974452e637502d399b89
Nathaniel Sena <nate.sena1@gmail.com>
2018-03-06 13:32:01 -0800

    just pushing changes

--- client/src/Components/Aframe/AframeComponents/AframePost.js
+++ client/src/Components/Aframe/AframeComponents/AframePost.js
@@ -17,12 +17,12 @@ class AframePost extends React.Component{
     }
     //format position takes inputs to move things around
     formatPosition(x,y,z, positionString){
-        // console.log("pos: ", positionString)
+        console.log("pos: ", positionString)
         var coordinates = positionString.split(' ')
         coordinates[0] = Number(coordinates[0]) + x
         coordinates[1] = Number(coordinates[1]) + y
         coordinates[2] = Number(coordinates[2]) + z
-        // console.log("coordinates: ", coordinates)
+        console.log("coordinates: ", coordinates)
         return coordinates.join(" ")
     }
     click(){
@@ -30,6 +30,9 @@ class AframePost extends React.Component{
         this.setState({
             color: "red"
         })
+    }
+    onHover(){
+        console.log("hovering")
     }
     render(){
         let featuredImage = null
@@ -41,8 +44,11 @@ class AframePost extends React.Component{
                 <a-text value={`${this.props.title}`} color="#000000" align="center" width="2" position={this.formatPosition(0, 0.9, 0,this.props.position)} rotation={this.props.rotation}></a-text>
                 <a-text value={`${this.props.description}`} color="#000000" align="center" width="2" position={this.formatPosition(0, 0.75, 0,this.props.position)} rotation={this.props.rotation}></a-text>
                 {featuredImage}
-                <a-plane height="2" width="2" color={this.state.color} position={this.props.position} rotation={this.props.rotation}></a-plane>
+                <a-plane height="2" width="2" color={this.state.color} position={this.props.position} rotation={this.props.rotation}>
+                </a-plane>
                 <a-plane height="2" width="2" color={this.state.color} position={this.props.position} rotation={this.reverseRotation(this.props.rotation)}></a-plane>
+                {/* <a-animation begin="mouseenter" end="mouseleave" attribute="position" direction="alternate" from={this.props.position} to={this.formatPosition(0, .1, 0, this.props.position)} dur="1500" repeat="indefinite">
+                </a-animation> */}
             </a-entity>
         )
     }
